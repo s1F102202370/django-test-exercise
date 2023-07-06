520e8a521c2aa3bbed5c289e6f60421ed95866e4
1F10220237 <s1f102202370@iniad.org>
2023-07-06 17:40:32 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
-        self.assertEqual(1+2, 3)
+        self.assertEqual(1 + 2, 3)
 
 
 class TaskModelTestCase(TestCase):
