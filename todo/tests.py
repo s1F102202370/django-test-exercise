5c029c0c53d02bf44a67e8914484431442397aca
1F10220237 <s1f102202370@iniad.org>
2023-07-06 17:36:56 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -6,7 +6,7 @@ from todo.models import Task
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
-        self.assertEqual(1 + 2, 3)
+        self.assertEqual(1+2, 3)
 
 
 class TaskModelTestCase(TestCase):
